66050e73ba88c71416df2eaf2263d6fbd18c1a87
Tyler Lampel <lampel13@gmail.com>
2023-03-31 15:47:10 -0400

    adding authorization to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+    include ActionController::Cookies
+
+    before_action :authorize
+
+    private
+
+    def current_user
+        @current_user ||= session[:user_id] &&
+        User.find_by(id: session[:user_id])
+    end
+
+    def authorize
+        return render json: { errors: ["Not Authorized"] }, status: :unauthorized unless session.include? :user_id
+    end
 end
