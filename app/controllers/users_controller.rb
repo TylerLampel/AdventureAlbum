c62c312e77547aa952ceb083e3baa9e1e7b5f289
Tyler Lampel <lampel13@gmail.com>
2023-03-31 15:41:23 -0400

    adding user login to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.user_id
+        render json: user
+    end
+
+    def show 
+        user = User.find(session[:user_id])
+        render json: user
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end 
 end
