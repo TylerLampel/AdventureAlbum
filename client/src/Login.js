babcc5ed1833d874a278c29c2cd4de745d2dc550
Tyler Lampel <lampel13@gmail.com>
2023-04-07 12:01:28 -0400

    Adding login and signup forms

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { UserContext } from "./context/User";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-  return <div></div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const { login } = useContext(UserContext);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((user) => {
+        if (!user.errors) {
+          login(user);
+          navigate("/");
+        } else {
+          setUsername("");
+          setPassword("");
+          setError(user.errors);
+        }
+      });
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username-input">Username: </label>
+        <input
+          id="username-input"
+          label="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br />
+        <label htmlFor="password-input">Password: </label>
+        <input
+          id="password-input"
+          label="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button type="submit">Log In</button>
+      </form>
+      <ul>
+        {error && (
+          <p severity="error" onClose={() => setError(!error)}>
+            {error}
+          </p>
+        )}
+      </ul>
+    </div>
+  );
 }
 
 export default Login;
--- client/src/Signup.js
+++ client/src/Signup.js
@@ -7,9 +7,74 @@ function Signup() {
   const [password, setPassword] = useState("");
   const [passwordConfirmation, setPasswordConfirmation] = useState("");
   const [errorsList, setErrorsList] = useState([]);
+  const { signup } = useContext(UserContext);
   const navigate = useNavigate();
 
-  return <div></div>;
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+        password_confirmation: passwordConfirmation,
+      }),
+    })
+      .then((res) => res.json())
+      .then((user) => {
+        if (!user.errors) {
+          signup(user);
+          navigate("/");
+        } else {
+          setUsername("");
+          setPassword("");
+          setPasswordConfirmation("");
+          const errorList = user.errors.map((e) => <div>{e}</div>);
+          setErrorsList(errorList);
+        }
+      });
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username: </label>
+        <input
+          id="username"
+          label="Username"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br />
+        <label htmlFor="password-input">Password: </label>
+        <input
+          id="password-input"
+          label="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <label htmlFor="password-confirmation-input">
+          Password Confirmation:{" "}
+        </label>
+        <input
+          id="password-confirmation-input"
+          label="Password Confirmation"
+          type="password"
+          value={passwordConfirmation}
+          onChange={(e) => setPasswordConfirmation(e.target.value)}
+        />
+        <br />
+        <button type="submit">Sign Up</button>
+        <p>{errorsList}</p>
+      </form>
+    </div>
+  );
 }
 
 export default Signup;
